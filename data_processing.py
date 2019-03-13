816cac73f463aac0a2621ed66da93df4122bc1c3
xiaosen <276032204@qq.com>
2019-03-13 12:26:01 +0800

    add corpus and data_processing.py

---
+++ data_processing.py
@@ -0,0 +1,62 @@
+import os
+import csv
+import numpy as np
+
+
+def get_line(file_path):
+    f = open(file_path, 'r', errors='ignore', encoding='GB2312')
+    line = f.readline()
+    line = line.strip('\n')
+
+    line.encode("utf-8")
+
+    f.close()
+    return line
+
+
+if __name__ == '__main__':
+    root_dir = 'data/2000/'     
+
+    folders = ['neg/', 'pos/']
+
+    train_path = './data/' + 'train' + '.tsv'
+    train = open(train_path, 'w', encoding='utf-8', newline='')
+    train_writer = csv.writer(train, delimiter='\t')
+
+    dev_path = './data/' + 'dev' + '.tsv'
+    dev = open(dev_path, 'w', encoding='utf-8', newline='')
+    dev_writer = csv.writer(dev, delimiter='\t')
+
+    test_path = './data/' + 'test' + '.tsv'
+    test = open(test_path, 'w', encoding='utf-8', newline='')
+    test_writer = csv.writer(test, delimiter='\t')
+
+    length_list = []
+
+    for folder in folders:
+        input_path = root_dir + folder
+
+        i = 0
+
+        for parent, directories, files in os.walk(input_path):
+
+            for file in files:
+                content = get_line(input_path + file)
+                length_list.append(len(content))
+
+                if i < 800:
+                    train_writer.writerow([content, folder])
+                elif 800 <= i < 900:
+                    dev_writer.writerow([content, folder])
+                else:
+                    test_writer.writerow([content, folder])
+
+                i = i + 1
+
+    train.close()
+    dev.close()
+    test.close()
+
+    print(np.max(length_list))
+    print(np.mean(length_list))
+
